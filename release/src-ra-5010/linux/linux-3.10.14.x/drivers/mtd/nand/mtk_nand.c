d8f35aaf032c6024799fc3adfa7baddbae3cc6d7
marius-kob <105521204+marius-kob@users.noreply.github.com>
2022-05-28 23:27:06 +0200

    Update mtk_nand.c

--- release/src-ra-5010/linux/linux-3.10.14.x/drivers/mtd/nand/mtk_nand.c
+++ release/src-ra-5010/linux/linux-3.10.14.x/drivers/mtd/nand/mtk_nand.c
@@ -5049,7 +5049,7 @@ int mtk_nand_probe()
 	g_pasStatic_Partition[6].size = 0xa000000;
 #endif
 // Change offset if all sectors in Config partition are bad
-    if ((fact_bbt[3] & 0xf0) == 0xf0)
+    if (true)
         g_pasStatic_Partition[1].offset = 0x180000;
 // Xiaomi
 	err = add_mtd_partitions(mtd, g_pasStatic_Partition, part_num);
